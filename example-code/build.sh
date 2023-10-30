e1e24d5bcf66f9db71896276e1bf47e966c343b5
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-10-30 11:49:45 +0000

    Clean up after ourselves.

--- example-code/build.sh
+++ example-code/build.sh
@@ -5,12 +5,15 @@ set -euo pipefail
 # Check the example code
 pushd ./native
 cargo build --all
+cargo clean
 popd
 # And the C based example
 pushd native/ffi/use-rust-in-c
 make
+make clean
 popd
 # And the nRF52 examples
 pushd ./nrf52/bsp_demo
 cargo build
+cargo clean
 popd
