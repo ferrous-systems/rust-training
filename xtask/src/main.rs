328a963d7c05c0c98a8f8fedecb7584a777d5ec9
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2024-06-10 16:04:31 -0600

    setup xtask infra for cheatsheets

---
+++ xtask/src/main.rs
@@ -0,0 +1,37 @@
+#![deny(warnings)]
+
+mod tasks;
+
+use std::env;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    // first arg is the name of the executable; skip it
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let args = args.iter().map(|arg| &arg[..]).collect::<Vec<_>>();
+
+    match &args[..] {
+        ["make-cheatsheet", lang] => tasks::make_cheatsheet(lang),
+        ["test-cheatsheet", lang] => tasks::test_cheatsheet(lang),
+        _ => {
+            eprintln!(
+                "cargo xtask
+Setup a programming language cheatsheet with the `training-slides` files structure.
+Test all entries are in sync with `SUMMARY.md`.
+
+USAGE:
+    cargo xtask [COMMAND]
+
+COMMANDS:
+    make-cheatsheet [LANG]      make LANG cheatsheet by scraping slides names in `SUMMARY.md`
+    test-cheatsheet [LANG]      test LANG's cheatsheet (all `SUMMARY.md` items are in sheet and viceversa)
+    
+",
+            );
+
+            Ok(())
+        }
+    }
+}
+
---
+++ xtask/src/tasks.rs
@@ -0,0 +1,12 @@
+// dummy
+pub fn make_cheatsheet(lang: &str) -> color_eyre::Result<()> {
+    println!("make_cheatsheet for {lang}");
+    Ok(())
+
+}
+
+pub fn test_cheatsheet(lang: &str) -> color_eyre::Result<()> {
+    println!("make_cheatsheet for {lang}");
+    Ok(())
+
+}
