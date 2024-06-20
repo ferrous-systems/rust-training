3bafd2076e0035b534ddaf066e7fb6936a2ba776
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-06-20 15:59:16 +0100

    Vendored dependencies for armv8-r demo.
    Now we can build without cargo, even with dependencies.

--- example-code/qemu-armv8r/qemu.sh
+++ example-code/qemu-armv8r/qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 TARGET_DIR=target/production
-BINARY=${TARGET_DIR}/qemu-demo
+BINARY=${1:-${TARGET_DIR}/no-heap}
 qemu-system-arm -machine mps3-an536 -cpu cortex-r52 -semihosting -nographic -kernel ${BINARY}
