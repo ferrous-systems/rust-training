b457bf86c56a4ef7ba9aaabea5b742f2a8d8420b
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-06-28 11:58:48 +0100

    Expand the FFI example
    Box<T> and T* have the same representation, if T: Sized

--- example-code/native/ffi/use-rust-in-c/example.h
+++ example-code/native/ffi/use-rust-in-c/example.h
@@ -10,3 +10,9 @@ extern magic_adder_t magicadder_new(uint32_t amount);
 
 /// Wraps MagicAdder::process_value
 extern uint32_t magicadder_process_value(magic_adder_t *self, uint32_t value);
+
+/// Heap allocate a new magic_adder_t
+magic_adder_t* magicadder_allocate(uint32_t amount);
+
+/// Destroy a magic_adder_t that was created with `magicadder_allocate`
+void magicadder_free(magic_adder_t* p_adder);
