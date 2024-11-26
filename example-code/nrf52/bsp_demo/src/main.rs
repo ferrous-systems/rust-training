e366884f0e2e33e5ce0fc90ea5c0d8380091a1e7
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2024-11-26 10:36:58 +0000

    bsp-demo: Slow down the blink

--- example-code/nrf52/bsp_demo/src/main.rs
+++ example-code/nrf52/bsp_demo/src/main.rs
@@ -1,18 +1,21 @@
 #![no_std]
 #![no_main]
 
-use nrf52840_dk_bsp::Board;
-use cortex_m_rt::entry;
 use core::fmt::Write;
+use cortex_m_rt::entry;
+use nrf52840_dk_bsp::{hal, Board};
 
 #[entry]
 fn main() -> ! {
     let mut nrf52 = Board::take().unwrap();
+    let mut timer = hal::Timer::new(nrf52.TIMER0);
     loop {
         writeln!(nrf52.cdc, "On!").unwrap();
         nrf52.leds.led_2.enable();
+        timer.delay(1_000_000);
         writeln!(nrf52.cdc, "Off!").unwrap();
         nrf52.leds.led_2.disable();
+        timer.delay(1_000_000);
     }
 }
 
@@ -22,4 +25,3 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
         cortex_m::asm::udf();
     }
 }
-
