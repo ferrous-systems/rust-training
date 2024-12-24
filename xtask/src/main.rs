31a441b4fe73a49c54e50fd8798fe684186b0c00
mrg <miguelraz@ciencias.unam.mx>
2024-12-24 00:11:00 -0600

    reformat panic message, move HELP_TEXT to static and use an array instead of a vector in main.rs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,29 +1,10 @@
-//#![deny(warnings)]
+#![deny(warnings)]
 
 mod tasks;
 
 use std::env;
 
-// Code adapted from the xtask workflow in rust-exercises
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-
-    // first arg is the name of the executable; skip it
-    let args = env::args().skip(1).collect::<Vec<_>>();
-    let args = args.iter().map(|arg| &arg[..]).collect::<Vec<_>>();
-
-    let langs = vec!["python", "go", "cpp", "swift", "java", "julia", "c"];
-
-    if !langs.contains(&args[1]) {
-        panic!("{} is not a valid language name. \nExpected one of: python, go, cpp, swift, java, julia, c", &args[1]);
-    }
-
-    match &args[..] {
-        ["make-cheatsheet", lang] => tasks::make_cheatsheet(lang),
-        ["test-cheatsheet", lang] => tasks::test_cheatsheet(lang),
-        _ => {
-            eprintln!(
-                "cargo xtask
+static HELP_TEXT: &'static str = "cargo xtask
 
 USAGE:
     cargo xtask [COMMAND]
@@ -34,10 +15,31 @@ COMMANDS:
 
 LANG:
     Valid values are `python, go, cpp, swift, java, julia, c`
-",
-            );
+";
+
+// Code adapted from the xtask workflow in rust-exercises
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    // first arg is the name of the executable; skip it
+    let unprocessed_args = env::args().skip(1).collect::<Vec<_>>();
+    let args: Vec<&str> = unprocessed_args.iter().map(|arg| &arg[..]).collect::<Vec<&str>>();
+
+    let langs = ["python", "go", "cpp", "swift", "java", "julia", "c"];
+
+    if !langs.contains(&args[1]) {
+        let panic_text = format!("{} {}\n{}\n", args[1], "is not a valid language name. \n\nExpected one of:", langs.join("\n"));
+        panic!("{panic_text}");
+    }
+
+    match &args[..] {
+        ["make-cheatsheet", lang] => tasks::make_cheatsheet(lang),
+        ["test-cheatsheet", lang] => tasks::test_cheatsheet(lang),
+        _ => {
+            eprintln!("{HELP_TEXT}");
 
             Ok(())
         }
     }
 }
+
