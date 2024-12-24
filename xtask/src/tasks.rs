d8f30210c54cb14c7a5740471b14f126762b02a0
mrg <miguelraz@ciencias.unam.mx>
2024-12-24 00:13:36 -0600

    refactor unit tests into #[cfg(test)] mod tests {...}, cleaner and more eager panic handling

--- xtask/src/tasks.rs
+++ xtask/src/tasks.rs
@@ -24,17 +24,6 @@ fn get_slide_name(line: &str) -> String {
     String::from(&line[bot + 1..top])
 }
 
-#[test]
-fn test_get_slide_name() {
-    let test = "* [Methods and Traits](./methods-traits.md)";
-    let res = "Methods and Traits";
-    assert_eq!(res, get_slide_name(test));
-
-    let test2 = "* [Shared Mutability (Cell, RefCell)](./shared-mutability.md)";
-    let res2 = "Shared Mutability (Cell, RefCell)";
-    assert_eq!(res2, get_slide_name(test2));
-}
-
 const INITIAL_HEADER: &str = "# Rust Fundamentals";
 const LAST_HEADER: &str = "# No-Std Rust";
 
@@ -49,8 +38,12 @@ fn focus_regions(text: &str) -> Vec<Vec<String>> {
         panic!("YOUR LAST_HEADER is not part of the text input. CHECK your `SUMMARY.md` for {LAST_HEADER}");
     }
 
-    let first_header = text.find(INITIAL_HEADER).unwrap();
-    let last_header = text.rfind(LAST_HEADER).unwrap();
+    let Some(first_header) = text.find(INITIAL_HEADER) else {
+        panic!("Could not find the initial header {INITIAL_HEADER:?}. Check your `SUMMARY.md`.")
+    };
+    let Some(last_header) = text.rfind(LAST_HEADER) else {
+        panic!("Could not find the last header {LAST_HEADER:?}. Check your `SUMMARY.md`.")
+    };
 
     let text = &text[first_header..last_header];
 
@@ -76,49 +69,6 @@ fn focus_regions(text: &str) -> Vec<Vec<String>> {
     result
 }
 
-#[test]
-fn test_focus_regions() {
-    let test = "# Summary
-
-[Start Here](./start_here.md)
-
-# Rust Fundamentals
-
-* [Overview](./overview.md)
-
-# Applied Rust
-
-Using Rust on Windows/macOS/Linux. Requires [Rust Fundamentals](#rust-fundamentals).
-
-* [Methods and Traits](./methods-traits.md)
-
-# Advanced Rust
-
-Topics that go beyond [Applied Rust](#applied-rust).
-
-* [Advanced Strings](./advanced-strings.md)
-
-# No-Std Rust
-
-Rust for the Linux Kernel and other no-std environments with an pre-existing C API. Requires [Applied Rust](#applied-rust).
-";
-    let res = vec![
-        vec![
-            "# Rust Fundamentals".to_owned(),
-            "* [Overview](./overview.md)".to_owned(),
-        ],
-        vec![
-            "# Applied Rust".to_owned(),
-            "* [Methods and Traits](./methods-traits.md)".to_owned(),
-        ],
-        vec![
-            "# Advanced Rust".to_owned(),
-            "* [Advanced Strings](./advanced-strings.md)".to_owned(),
-        ],
-    ];
-    assert_eq!(focus_regions(test), res);
-}
-
 fn extract_slides(chunk: Vec<String>) -> SlidesSection {
     assert!(chunk.len() > 2);
     // # Rust Fundamentals
@@ -134,23 +84,6 @@ fn extract_slides(chunk: Vec<String>) -> SlidesSection {
         header,
         slide_titles,
     }
-}
-
-#[test]
-fn test_extract_slides() {
-    let test = "# Rust Fundamentals
-* [Rust I/O Traits](./io.md)
-* [Generics](./generics.md)
-# No-Std Rust";
-    let header = String::from("Rust Fundamentals");
-    let slide_titles = vec![String::from("Rust I/O Traits"), String::from("Generics")];
-    let res = SlidesSection {
-        header,
-        slide_titles,
-    };
-    let region = focus_regions(test);
-    assert_eq!(extract_slides(region[0].clone()), res);
-    assert!(true);
 }
 
 pub fn make_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
@@ -174,7 +107,6 @@ pub fn make_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
             drop(f.write_all(result_text.as_bytes()));
             eprintln!("Cheatsheat for {lang} written at {file_str}");
             eprintln!("Make sure to add it to SUMMARY.md!")
-
         }
         Err(_) => {
             eprintln!("File {lang}-cheatsheet.md already exists - checking it's in sync");
@@ -186,7 +118,8 @@ pub fn make_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
 
 pub fn test_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
     // Collect Vec<SlideSection>
-    let text = read_to_string("./training-slides/src/SUMMARY.md").expect("could not read_to_string - SUMMARY.md not found");
+    let text = read_to_string("./training-slides/src/SUMMARY.md")
+        .expect("could not read_to_string - SUMMARY.md not found");
     let slide_texts = focus_regions(&text);
     let slide_sections: Vec<SlidesSection> = slide_texts
         .iter()
@@ -198,7 +131,7 @@ pub fn test_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
     let cheatsheet_text = read_to_string(file_name).expect("lang-cheatsheet.md not found");
     let cheatsheet_lines = cheatsheet_text
         .lines()
-        // Tricky: We only care about entries that start with '#', so filtering for them is enough to get only the 
+        // Tricky: We only care about entries that start with '#', so filtering for them is enough to get only the
         // interesting lines!
         .filter(|l| l.starts_with("#"))
         .map(|l| l.to_string())
@@ -211,7 +144,7 @@ pub fn test_cheatsheet(lang: &str) -> Result<(), eyre::Report> {
             if line != cheatsheet_lines.first().unwrap() {
                 idx += 1;
                 // Check if people have added extra headers - leave them alone
-                // so that lang - specific advice doesn't have to correlate to slides 
+                // so that lang - specific advice doesn't have to correlate to slides
                 // if it goes at the end
                 if idx == slide_sections.len() {
                     eprintln!("Neat! {lang}-cheatsheet.md is in sync AND contains some extra info at the end");
@@ -257,4 +190,79 @@ fn write_cheatsheet(slide_sections: Vec<SlidesSection>) -> String {
         res.push_str(&section_str_buf);
     }
     res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_slide_name() {
+        let test = "* [Methods and Traits](./methods-traits.md)";
+        let res = "Methods and Traits";
+        assert_eq!(res, get_slide_name(test));
+
+        let test2 = "* [Shared Mutability (Cell, RefCell)](./shared-mutability.md)";
+        let res2 = "Shared Mutability (Cell, RefCell)";
+        assert_eq!(res2, get_slide_name(test2));
+    }
+
+    #[test]
+    fn test_focus_regions() {
+        let test = "# Summary
+
+[Start Here](./start_here.md)
+
+# Rust Fundamentals
+
+* [Overview](./overview.md)
+
+# Applied Rust
+
+Using Rust on Windows/macOS/Linux. Requires [Rust Fundamentals](#rust-fundamentals).
+
+* [Methods and Traits](./methods-traits.md)
+
+# Advanced Rust
+
+Topics that go beyond [Applied Rust](#applied-rust).
+
+* [Advanced Strings](./advanced-strings.md)
+
+# No-Std Rust
+
+Rust for the Linux Kernel and other no-std environments with an pre-existing C API. Requires [Applied Rust](#applied-rust).
+";
+        let res = vec![
+            vec![
+                "# Rust Fundamentals".to_owned(),
+                "* [Overview](./overview.md)".to_owned(),
+            ],
+            vec![
+                "# Applied Rust".to_owned(),
+                "* [Methods and Traits](./methods-traits.md)".to_owned(),
+            ],
+            vec![
+                "# Advanced Rust".to_owned(),
+                "* [Advanced Strings](./advanced-strings.md)".to_owned(),
+            ],
+        ];
+        assert_eq!(focus_regions(test), res);
+    }
+    #[test]
+    fn test_extract_slides() {
+        let test = "# Rust Fundamentals
+* [Rust I/O Traits](./io.md)
+* [Generics](./generics.md)
+# No-Std Rust";
+        let header = String::from("Rust Fundamentals");
+        let slide_titles = vec![String::from("Rust I/O Traits"), String::from("Generics")];
+        let res = SlidesSection {
+            header,
+            slide_titles,
+        };
+        let region = focus_regions(test);
+        assert_eq!(extract_slides(region[0].clone()), res);
+        assert!(true);
+    }
 }
