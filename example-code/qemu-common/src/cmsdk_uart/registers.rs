5299e0bba0eceb2e2502e7be1a0309a0093a0af9
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-09-04 15:21:26 +0100

    bitbybit can now generate defmt impls.

--- example-code/qemu-common/src/cmsdk_uart/registers.rs
+++ example-code/qemu-common/src/cmsdk_uart/registers.rs
@@ -1,7 +1,7 @@
 //! Register definitions for the CSMSDK UART
 
 /// UART Status
-#[bitbybit::bitfield(u32)]
+#[bitbybit::bitfield(u32, defmt_bitfields)]
 pub struct Status {
     /// TX Full
     #[bit(0, rw)]
@@ -17,18 +17,8 @@ pub struct Status {
     rxo: bool,
 }
 
-impl defmt::Format for Status {
-    fn format(&self, f: defmt::Formatter<'_>) {
-        defmt::write!(
-            f,
-            "Status {{ txf={0=0..1}, rxf={0=1..2}, txo={0=2..3}, rxo={0=3..4} }}",
-            self.raw_value()
-        )
-    }
-}
-
 /// UART Control
-#[bitbybit::bitfield(u32)]
+#[bitbybit::bitfield(u32, defmt_bitfields)]
 pub struct Control {
     /// TX Enabled
     #[bit(0, rw)]
@@ -50,18 +40,8 @@ pub struct Control {
     rxoie: bool,
 }
 
-impl defmt::Format for Control {
-    fn format(&self, f: defmt::Formatter<'_>) {
-        defmt::write!(
-            f,
-            "Control {{ txe={0=0..1} rxe={0=1..2} txie={0=2..3} rxie={0=3..4} txoie={0=4..5} rxoie={0=5..6} }}",
-            self.raw_value()
-        )
-    }
-}
-
 /// UART Interrupt Status
-#[bitbybit::bitfield(u32, default = 0)]
+#[bitbybit::bitfield(u32, default = 0, defmt_bitfields)]
 pub struct IntStatus {
     /// TX Interrupt
     #[bit(0, rw)]
@@ -75,16 +55,6 @@ pub struct IntStatus {
     /// RX Overflow Interrupt
     #[bit(3, rw)]
     rxoi: bool,
-}
-
-impl defmt::Format for IntStatus {
-    fn format(&self, f: defmt::Formatter<'_>) {
-        defmt::write!(
-            f,
-            "IntStatus {{ txi={0=0..1} rxi={0=1..2} txoi={0=2..3} rxoi={0=3..4} }}",
-            self.raw_value()
-        )
-    }
 }
 
 // End of file
