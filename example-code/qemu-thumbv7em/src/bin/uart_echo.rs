f81b110064189564ca7b7b6d9fc447a3b3d38959
Robin Mueller <robin.mueller.m@pm.me>
2025-10-17 17:11:15 +0200

    added asynchronous UART

--- example-code/qemu-thumbv7em/src/bin/uart_echo.rs
+++ example-code/qemu-thumbv7em/src/bin/uart_echo.rs
@@ -7,17 +7,14 @@ use core::cell::RefCell;
 use defmt_semihosting as _;
 
 use heapless::spsc::{Producer, Queue};
-use qemu_thumbv7em::{
-    interrupt,
-    interrupts::Interrupts,
-    uart::{self, UART0_ADDR},
-    SYSTEM_CLOCK,
-};
+use qemu_thumbv7em::{interrupt, interrupts::Interrupts, uart, SYSTEM_CLOCK};
 
 const QUEUE_SIZE: usize = 16;
 static QUEUE: static_cell::ConstStaticCell<Queue<u8, QUEUE_SIZE>> =
     static_cell::ConstStaticCell::new(Queue::<u8, QUEUE_SIZE>::new());
 
+static RX: critical_section::Mutex<RefCell<Option<uart::Rx>>> =
+    critical_section::Mutex::new(RefCell::new(None));
 static PRODUCER: critical_section::Mutex<RefCell<Option<Producer<'_, u8, QUEUE_SIZE>>>> =
     critical_section::Mutex::new(RefCell::new(None));
 
@@ -26,13 +23,15 @@ fn main() -> ! {
     defmt::info!("Running UART echo app. Connect on telnet 4321 to chat!");
 
     let peripherals = qemu_thumbv7em::Peripherals::take().unwrap();
-    let mut uart0 = uart::CmsdkUart::new(peripherals.uart0);
+    let mut uart0 = uart::Uart::new(peripherals.uart0);
     uart0.init(115200, SYSTEM_CLOCK).unwrap();
-    uart0.enable_rx_interrupt();
+    let (mut tx, mut rx) = uart0.split();
+    rx.enable_interrupts();
     let queue = QUEUE.take();
     let (prod, mut cons) = queue.split();
     critical_section::with(|cs| {
         PRODUCER.borrow(cs).replace(Some(prod));
+        RX.borrow(cs).replace(Some(rx));
     });
     unsafe {
         cortex_m::peripheral::NVIC::unmask(Interrupts::Uart0Rx);
@@ -41,7 +40,7 @@ fn main() -> ! {
 
     loop {
         if let Some(byte) = cons.dequeue() {
-            uart0.write_blocking(byte);
+            tx.write_blocking(byte);
         }
     }
 }
@@ -49,14 +48,18 @@ fn main() -> ! {
 /// Called when UART0 has a RX interrupt
 #[interrupt]
 fn Uart0Rx() {
-    let mut uart0 = unsafe { uart::CmsdkUart::new_with_raw_addr(UART0_ADDR) };
-    let int_status = uart0.read_int_status();
-    if let Ok(byte) = uart0.read() {
+    let opt_byte = critical_section::with(|cs| {
+        let mut rx_ref = RX.borrow(cs).borrow_mut();
+        let rx = rx_ref.as_mut().unwrap();
+        let byte = rx.read();
+        rx.clear_interrupts();
+        byte
+    });
+    if let Ok(byte) = opt_byte {
         critical_section::with(|cs| {
             if let Some(prod) = PRODUCER.borrow(cs).borrow_mut().as_mut() {
                 prod.enqueue(byte).unwrap();
             }
         });
     }
-    uart0.clear_interrupts(int_status);
 }
