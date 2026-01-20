0ab3241d33f3daf6be599fb677d2efb7378a2b0f
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-01-20 11:50:16 +0000

    Fix build.

--- example-code/qemu-aarch32v8r/src/lib.rs
+++ example-code/qemu-aarch32v8r/src/lib.rs
@@ -2,8 +2,15 @@
 
 // pull in defmt logger
 use defmt_semihosting as _;
+// pull in critical-section
+use aarch32_cpu as _;
 
 pub mod uart;
+
+/// The clock speed of the peripheral subsystem on an SSE-300 SoC an on MPS3 board.
+///
+/// Probably right for an MPS3-AN536
+pub const PERIPHERAL_CLOCK: u32 = 25_000_000;
 
 /// Called when the application raises an unrecoverable `panic!`.
 ///
