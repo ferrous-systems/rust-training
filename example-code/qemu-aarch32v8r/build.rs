21dee1456e13f42299705fab3fcae8fc95232e92
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-07-15 12:03:04 +0100

    Simplify other example build.rs files

--- example-code/qemu-aarch32v8r/build.rs
+++ example-code/qemu-aarch32v8r/build.rs
@@ -2,16 +2,11 @@
 //!
 //! This script only executes when using `cargo` to build the project.
 
-use std::io::Write;
-
 fn main() {
     // Put `memory.ld` file in our output directory and ensure it's on the
     // linker search path.
-    let out = &std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
-    std::fs::File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
+    let out = std::path::PathBuf::from(std::env::var_os("OUT_DIR").expect("Reading OUT_DIR"));
+    std::fs::write(out.join("memory.x"), include_bytes!("memory.x")).expect("Writing memory.x");
     println!("cargo:rerun-if-changed=memory.x");
     println!("cargo:rustc-link-arg=-Tlink.x");
     println!("cargo:rustc-link-arg=-Tdefmt.x");
